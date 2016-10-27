d57986fa056bc37bbb7d79a00154f2c79d6a0774
Thiago Vargas <tjvargas2@gmail.com>
2016-10-27 15:36:26 -0200

    Adequação para padrão de nomenclatura python, e criação de uma classe

--- sequencia.py
+++ sequencia.py
@@ -2,38 +2,48 @@
 #  Algoritmo que retorna a sequência de caracteres que mais aparecem em sequência
 #
 
-string = "glooooobbbbbo.commmmm.br";
+class Sequencia():
 
-charAnterior = '';
+    def __init__(self):
+        self.string = "gloooooobbbbbo.commmmm.br" #deve imprimir oooooo
+        ## var auxiliar
+        self.aux_char_anterior = ''
+        ## lista auxiliar das sequencias
+        self.sequencias = []
 
-sequencias = [];
+    # Adiciona caracteres na lista auxiliar, se repetir concatena
+    def add_sequencia(self, x):
+        ultima_posicao = len(self.sequencias)-1;
+        ultima_sequencia = self.sequencias[ultima_posicao];
+        ultimo_char = ultima_sequencia[len(ultima_sequencia)-1];
+        if ultimo_char == x :
+            self.sequencias[ultima_posicao] = ultima_sequencia + x;
 
+    # retorna a posição do maior item da lista
+    def maior_item_da_lista(self, seq):
+        maior = 0
+        i = 0
+        while i < len(seq):
+            if len(seq[i]) > len(seq[maior]):
+                maior = i
+            i = i + 1
+        return maior;
 
-def addSequencia(x):
-    ultimaPosicao = len(sequencias)-1;
-    ultimaSequencia = sequencias[ultimaPosicao];
-    ultimoChar = ultimaSequencia[len(ultimaSequencia)-1];
-    if ultimoChar == x :
-        sequencias[ultimaPosicao] = ultimaSequencia + x;
+    # percorre a string e adiciona na lista auxiliar.
+    def run(self):
+        for i in range(len(self.string)):
+            if i > 0:
+                self.char_anterior = self.string[i-1]
+                if self.string[i] == self.char_anterior:
+                    self.add_sequencia(self.string[i])
+                else:
+                    self.sequencias.append(self.string[i]);
+            else:
+                self.sequencias.append(self.string[i]);
 
+        # imprime a maior sequencia de caracteres. Empates imprimem a primeira da esquerda para a direita
+        print(self.sequencias[self.maior_item_da_lista(self.sequencias)]);
 
-def maiorSequencia(seq):
-    maior = 0
-    i = 0
-    while i < len(seq):
-        if len(seq[i]) > len(seq[maior]):
-            maior = i
-        i = i + 1
-    return maior;
+sequencia = Sequencia()
 
-for i in range(len(string)):
-    if i > 0:
-        charAnterior = string[i-1]
-        if string[i] == charAnterior:
-            addSequencia(string[i])
-        else:
-            sequencias.append(string[i]);
-    else:
-        sequencias.append(string[i]);
-
-print(sequencias[maiorSequencia(sequencias)]);
+sequencia.run()
